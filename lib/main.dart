fc3f91e190b93c6bac0718dd549997dc581cae64
FalatM <73362691+FalatM@users.noreply.github.com>
2020-11-10 21:47:23 -0500

    Added assets and worked on layout.

--- lib/main.dart
+++ lib/main.dart
@@ -12,61 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[800],
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.deepOrange[700],
-                child: Text('Container 1'),
-
-              ),
-              Container(
-                width: 37,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow[700],
-                    child: Text('Container 2'),
-
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green[800],
-                    child: Text('Container 3'),
-
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    width: 37,
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    width: 100.0,
-                    color: Colors.blue[800],
-                    child: Text('Container 4'),
-
-                  ),
-                ],
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/mel.jpeg'),
               ),
             ],
           ),
